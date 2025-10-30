263c1a190e03c849b80656d70a1d26e266932d4f
kleythem voulany <kleythemvoulany@gmail.com>
2025-10-30 18:31:19 +0000

    يظل جاري رفع الصور وعلى الصفر وجاري النشر ولا يتم شيءا

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1088003864853:web:a68fe26ca939519b6fd11c",
   "apiKey": "AIzaSyA3mmRUpfh1ZaI1xc49yD52PSjVMZvRPVc",
   "authDomain": "studio-5464459884-856d3.firebaseapp.com",
-  "storageBucket": "studio-5464459884-856d3.appspot.com",
+  "storageBucket": "alboursa-cars.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1088003864853"
 };
